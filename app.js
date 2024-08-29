cd11b5ecefaa91a59d47db5583aa5cae8170e762
IsratTania <ij.tania61@gmail.com>
2024-08-29 21:12:29 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const playGame = (userChoice) => {
+    console.log("user choice = ",userChoice);
+
+}
+
+choices.forEach((choice) =>{
+    console.log(choice);
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        console.log("choice was clicked",userChoice);
+        playGame(userChoice);s
+    });
+});

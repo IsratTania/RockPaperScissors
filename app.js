56b897c5e4f0c099a0a141940533a28812c484f8
IsratTania <ij.tania61@gmail.com>
2024-10-27 19:10:07 +0600

    app.js
    gaming result show

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const comScorePara = document.querySelector("#comp-score");
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -10,20 +12,27 @@ const genCompChoice = () => {
     return options[randIdx];
 };
 
+
+
+
 const drawGame = () => {
-    console.log("game was draw");
+    //console.log("game was draw");
     msg.innerText = "Game was Draw. Play again.";
     msg.style.backgroundColor = "#081b31";
 }
 
 const showWinner = (userWin, userChoice, compChoice) => {
     if(userWin){
-        console.log("You Win");
+        userScore++;
+        userScorePara.innerText = userScore;
+        //console.log("You Win");
         msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
     }
     else{
-        console.log("You lose");
+        compScore++;
+        comScorePara.innerText = compScore;
+        //console.log("You lose");
         msg.innerText = `You Lost. ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red";
     }
